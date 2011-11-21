a5ffdcaa0a515a77f42718fe6955d4cf1631148d
peniel <peniel@peniel>
2011-11-21 15:58:37 -0500

    Test simple Chinese characters

--- protected/messages/cn_hant_tw/jobs.php
+++ protected/messages/cn_hant_tw/jobs.php
@@ -1,6 +1,5 @@
 <?php
 return array(
-    'First page' => '第一页',
     'Prev' => '上一頁',
     'Next' => '下一頁',
     'I want be employed' => "我要求職",
---
+++ protected/messages/zh_cn/jobs.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+    'Prev' => '下一页',
+    'Next' => '下一页',
+    'I want be employed' => "我要求职",
+    'I want employ' => "我要请人",
+    'All areas' => '全部地区',
+);
