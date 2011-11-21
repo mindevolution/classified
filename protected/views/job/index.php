da3e4d0375705b8a62f2476f4619923cda8c319b
peniel <peniel@peniel>
2011-11-21 13:13:28 -0500

    Add two partial view: job_job and tag

--- protected/views/job/index.php
+++ protected/views/job/index.php
@@ -78,19 +78,8 @@ foreach ($areas as $id => $name) {
 <!--left-part end-->
 <!--right-part start-->
 <div id="right-part">
-        <div class="key">
-		<a href="#">全職</a> &nbsp;<a href="#">兼職</a> &nbsp;<a href="#">餐館</a> &nbsp;<a href="#">美容</a> &nbsp;<a href="#">保姆</a>
-        </div>
-        <div class="j-joy">
-		<div class="menu-r">
-			<div class="more"><a href="#">&nbsp;</a></div>
-			<div class="joy-title"><a href="#">開心職場</a></div>
-		</div>
-		<dl>
-			<dt><a href="#">加薪的理由...</a></dt>
-			<dd>"你必须给我加薪！"一个人对他的老板说，"现在有三家公司找我呢。""是吗？"老板问他，"是哪三家公司找你？""电力...<span class="green12"><a href="#">查看詳細</a></span></dd>
-		</dl>
-        </div>
+	<?php echo $this_>renderPartial('_tag'); ?>
+	<?php echo $this->renderPartial('_job_joy'); ?>
 	<?php echo $this->renderPartial('right/job_board'); ?>
 	<?php echo $this->renderPartial('right/interactive_and_info'); ?>
 </div>
