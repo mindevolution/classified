da2de6ec0588a6eac022ac2af267ce350436573e
peniel <peniel@peniel>
2011-11-22 17:06:50 -0500

    Add traditional Chinese language translation

---
+++ protected/messages/zh_tw/jobs.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+    'Prev' => '上一頁',
+    'Next' => '下一頁',
+    'I want be employed' => "我要求職",
+    'I want employ' => "我要請人",
+    'All areas' => '全部地區',
+    'One word to find job' => '一句話找工作',
+    'Get a new code' => '刷新',
+    'Input verify code' => '請輸入驗證碼',
+	'Description' => '描述',
+);
