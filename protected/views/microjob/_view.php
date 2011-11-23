81527f5f6a33bb7a05709d58f8b7cbff4138694e
peniel <peniel@peniel>
2011-11-23 09:56:16 -0500

    Fix the micro job display style

--- protected/views/microjob/_view.php
+++ protected/views/microjob/_view.php
@@ -1,16 +1,32 @@
-<div class="view">
+<?php
+$controller = strtolower(substr(get_class($this), 0, 8));
+$show_day = false;
+$day = date('M d Y', strtotime($data->timestamp));
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
-	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
-	<br />
+$flash_datas = Yii::app()->user->getFlashes();
+if (isset($flash_datas['lastjob_day']) && $flash_datas['lastjob_day'] != $day) {
+	$show_day = true;
+}
+?>
+<?php if ($show_day || $index == 0): ?>
+	<div class="day-lists">
+		<ul>
+			<p class="list-date"><?php print $day; ?></p>
+		</ul>
+	</div>
+<?php endif; ?>
+<div class="joblist-row <?php echo $index % 2 ? '' : 'odd'; ?>">
+	<?php
+	echo CHtml::link(CHtml::encode($data->description), CController::createUrl($controller.'/view', array('id' => $data->id)));
+	?>
+	<?php
+	if ($day == date('M d Y')): ?>
+		<?php echo CHtml::image("images/classified/icon-new.gif", "new-icon"); ?>
+	<?php endif; ?>
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('description')); ?>:</b>
-	<?php echo CHtml::encode($data->description); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('timestamp')); ?>:</b>
-	<?php echo CHtml::encode($data->timestamp); ?>
-	<br />
-
-
+	<?php
+	if ($data->timestamp) {
+		Yii::app()->user->setFlash('lastjob_day', $day);
+	}
+	?>
 </div>
