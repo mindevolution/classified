9d3c6e143228b811da6620dceeae56ac378ec9cc
peniel <peniel@peniel>
2011-11-23 08:50:32 -0500

    Update the job layout

--- protected/views/layouts/job.php
+++ protected/views/layouts/job.php
@@ -1,13 +1,3 @@
 <?php $this->beginContent('//layouts/job_main'); ?>
-<div class="container">
-	<div class="span-19">
-		<div id="content">
-			<?php echo $content; ?>
-		</div><!-- content -->
-	</div>
-	<div class="span-5 last">
-		<div id="sidebar">
-		</div><!-- sidebar -->
-	</div>
-</div>
+<?php echo $content; ?>
 <?php $this->endContent(); ?>
