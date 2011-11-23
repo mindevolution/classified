5b8d797d92a65f5fb4786150d6fb318e5c8225b2
peniel <peniel@peniel>
2011-11-23 10:19:40 -0500

    Fix the job view

--- protected/views/job/view.php
+++ protected/views/job/view.php
@@ -8,45 +8,71 @@ $this->breadcrumbs = array(
 	<?php echo $messages; ?>
 <?php endif; ?>
 <!--detail start-->
-<div class="detail">
-	<h1 class="d-title"><?php echo $model->title; ?> </h1>
-	<div class="d-sub-title">(<?php echo CHtml::link($model->area->name, array('job/index', 'aid' => $model->area->aid)); ?>) &nbsp;
-		<?php echo $model->category->name; ?>/<?php echo $model->purpose->name; ?> &nbsp;
-		<?php echo date('Y年m月d日 H:m', strtotime($model->timestamp)); ?>
-	</div>
-	<p>
-		<?php echo $model->description; ?>
-	</p>
-	<div class="d-icons">
-		<div class="right">
-			<span class="edit">
-				<?php
-				echo CHtml::link('編輯', "", // the link for open the dialog
-					array(
-					'style' => 'cursor: pointer;',
-					'onclick' => "{authEdit(); $('#dialogAuth').dialog('open');}"));
-				?>
-				</span> &nbsp;&nbsp;&nbsp;
-				<span class="delete">
-				<?php
-				echo CHtml::link('刪除', "", // the link for open the dialog
-					array(
-					'style' => 'cursor: pointer;',
-					'onclick' => "{auth(); $('#dialogAuth').dialog('open');}"));
-				?>
-			</span>
+<div id="left-part">
+	<!--buttons start-->
+        <div class="buttons">
+		<?php echo CHtml::link(CHtml::image("images/classified/bt-needjob.gif", Yii::t('jobs', 'I want be employed'), array('class' => 'img-left')), array('job/create', 'pid' => 2)); ?>
+		<?php echo CHtml::link(CHtml::image("images/classified/bt-needp.gif",  Yii::t('jobs', 'I want employ')), array('job/create', 'pid' => 1)); ?>
+        </div>	
+	<?php if (isset($this->breadcrumbs)): ?>
+		<?php
+		$this->widget('zii.widgets.CBreadcrumbs', array(
+		    'links' => $this->breadcrumbs,
+		    'separator' => '&nbsp;&nbsp;&nbsp;>&nbsp;&nbsp;&nbsp;',
+		    'homeLink' => false,
+		));
+		?><!-- breadcrumbs -->
+	<?php endif ?>
+	<!--guide end-->
+	<!--buttons end-->
+
+	<div class="detail">
+		<h1 class="d-title"><?php echo $model->title; ?> </h1>
+		<div class="d-sub-title">(<?php echo CHtml::link($model->area->name, array('job/index', 'aid' => $model->area->aid)); ?>) &nbsp;
+			<?php echo $model->category->name; ?>/<?php echo $model->purpose->name; ?> &nbsp;
+			<?php echo date('Y年m月d日 H:m', strtotime($model->timestamp)); ?>
 		</div>
-		<?php if (isset($prevJob) && $prevJob): ?>
-			<span class="past-m"><?php echo CHtml::link('上一條', array('job/view', 'id' => $prevJob->id)); ?></span> &nbsp;
-		<?php endif; ?>
-		<?php if (isset($nextJob) && $nextJob): ?>
-			<span class="next-m"><?php echo CHtml::link('下一條', array('job/view', 'id' => $nextJob->id)); ?></span> 
-		<?php endif; ?>
-		<div style="clear:both;"></div>
+		<p>
+			<?php echo $model->description; ?>
+		</p>
+		<div class="d-icons">
+			<div class="right">
+				<span class="edit">
+					<?php
+					echo CHtml::link('編輯', "", // the link for open the dialog
+						array(
+						'style' => 'cursor: pointer;',
+						'onclick' => "{authEdit(); $('#dialogAuth').dialog('open');}"));
+					?>
+					</span> &nbsp;&nbsp;&nbsp;
+					<span class="delete">
+					<?php
+					echo CHtml::link('刪除', "", // the link for open the dialog
+						array(
+						'style' => 'cursor: pointer;',
+						'onclick' => "{auth(); $('#dialogAuth').dialog('open');}"));
+					?>
+				</span>
+			</div>
+			<?php if (isset($prevJob) && $prevJob): ?>
+				<span class="past-m"><?php echo CHtml::link('上一條', array('job/view', 'id' => $prevJob->id)); ?></span> &nbsp;
+			<?php endif; ?>
+			<?php if (isset($nextJob) && $nextJob): ?>
+				<span class="next-m"><?php echo CHtml::link('下一條', array('job/view', 'id' => $nextJob->id)); ?></span> 
+			<?php endif; ?>
+			<div style="clear:both;"></div>
+		</div>
+		<div class="share">
+			<img src="images/share.gif" />
+		</div>
 	</div>
-	<div class="share">
-		<img src="images/share.gif" />
-	</div>
+
+</div>
+
+<div id="right-part">
+	<?php echo $this->renderPartial('../jobInclude/right/_tag_cloud'); ?>
+	<?php echo $this->renderPartial('../jobInclude/right/_job_joy'); ?>
+	<?php echo $this->renderPartial('../jobInclude/right/_interactive_and_info'); ?>
 </div>
 
 <?php
